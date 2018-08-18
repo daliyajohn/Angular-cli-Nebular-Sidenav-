5f097dae3f1b049d0005ce44cfc101870c1fe36a
daliyajohn <daliyajohn75@gmail.com>
2018-08-18 13:54:10 +0530

    Add nebular configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NbThemeModule } from '@nebular/theme';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NbThemeModule.forRoot({ name: 'default' })
   ],
   providers: [],
   bootstrap: [AppComponent]
